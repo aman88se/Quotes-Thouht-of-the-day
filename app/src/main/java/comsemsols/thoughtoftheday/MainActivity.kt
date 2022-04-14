3faba7b40fb58ecdf486f3d29334fd5d80662dbe
amans <amansemwal542@gmail.com>
2022-04-14 19:40:01 +0900

    Learning ViewModel & ViewModel Factory

---
+++ app/src/main/java/comsemsols/thoughtoftheday/MainActivity.kt
@@ -0,0 +1,11 @@
+package comsemsols.thoughtoftheday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
