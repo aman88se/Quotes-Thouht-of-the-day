ce1debbc747f029c385d0bcdb383fc58a7c3e757
amans <amansemwal542@gmail.com>
2022-04-19 11:33:03 +0900

    Changed the UI part

--- app/src/main/java/comsemsols/thoughtoftheday/MainActivity.kt
+++ app/src/main/java/comsemsols/thoughtoftheday/MainActivity.kt
@@ -2,10 +2,26 @@ package comsemsols.thoughtoftheday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
+
+    fun onPrevious(view: View) {
+
+    }
+    fun onNext(view: View) {
+
+    }
+
+    fun onShare(view: View) {}
 }
