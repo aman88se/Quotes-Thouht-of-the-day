8eeb2607cb7df0ee257a477588eccb8cd9b9b52a
amans <amansemwal542@gmail.com>
2022-04-20 19:54:12 +0900

    Final App

--- app/src/main/java/comsemsols/thoughtoftheday/MainActivity.kt
+++ app/src/main/java/comsemsols/thoughtoftheday/MainActivity.kt
@@ -1,27 +1,52 @@
 package comsemsols.thoughtoftheday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
+
+    private val quoteText: TextView
+    get() = findViewById(R.id.quoteText)
+
+    private val quoteAuthor: TextView
+        get() = findViewById(R.id.quoteAuthor)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this,MainViewModelFactory(application)).get(MainViewModel::class.java)
+        setQuote(mainViewModel.getCurrentQuote())
 
 
 
+    }
 
+    private fun setQuote(quoteData: QuoteData){
+        quoteText.text = quoteData.text
+        quoteAuthor.text = quoteData.author
     }
 
     fun onPrevious(view: View) {
+        setQuote(mainViewModel.prevQuote())
 
     }
     fun onNext(view: View) {
+        setQuote(mainViewModel.nextQuote())
 
     }
 
-    fun onShare(view: View) {}
+    fun onShare(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.setType("text/plain")
+        intent.putExtra(Intent.EXTRA_TEXT,mainViewModel.getCurrentQuote().text)
+        startActivity(intent)
+    }
 }
--- app/src/main/java/comsemsols/thoughtoftheday/MainViewModel.kt
+++ app/src/main/java/comsemsols/thoughtoftheday/MainViewModel.kt
@@ -1,4 +1,38 @@
 package comsemsols.thoughtoftheday
 
-class MainViewModel {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import java.nio.charset.Charset
+
+class MainViewModel(val context: Context) : ViewModel() {
+
+    private var thoughtList: Array<QuoteData> = emptyArray()
+    private var index = 0
+
+    init {
+        thoughtList = loadQuoteFromAssets()
+    }
+
+    private fun loadQuoteFromAssets(): Array<QuoteData> {
+        val inputStream = context.assets.open("quotes.json")
+        val size : Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val json = String(buffer, Charsets.UTF_8)
+        val gson = Gson()
+        return gson.fromJson(json,Array<QuoteData>::class.java)
+
+    }
+
+    fun getCurrentQuote() = thoughtList[index]
+
+
+    fun nextQuote() = thoughtList[++index]
+
+    fun prevQuote() = thoughtList[--index]
+
+
+
 }
